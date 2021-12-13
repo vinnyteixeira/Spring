1cd560c9fe5c35ef2bc07a9587c297f34fb14999
Vinny Teixeira <87780451+vinnyteixeira@users.noreply.github.com>
2021-12-13 22:52:32 +0300

    versão 1

---
+++ blogpessoal com security/src/main/java/com/example/blogpessoal/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.example.blogpessoal.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.blogpessoal.postagem.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+	
+}
